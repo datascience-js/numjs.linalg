999b3a3e0d43642e8eb51ef2dd0ebf0150c430fc
shirihaim <shirihaim@gmail.com>
2015-11-26 18:49:51 +0200

    Merge with stass

--- numjs.linalg.js
+++ numjs.linalg.js
@@ -1,28 +1,28 @@
 var linalg = require('./build/Release/numjs.linalg');
 
 var numjs_linalg = {
-    Matrix: function(array, rows, cols){
-    this.rows = rows;
-    this.cols = cols;
-    this.data = new Float64Array(rows*cols);
+    Matrix: function (array, rows, cols) {
+        this.rows = rows;
+        this.cols = cols;
+        this.data = new Float64Array(rows * cols);
 
-    if(array.length === 0){
-        for(var j = 0; j < rows*cols; j++){
-            this.data[j] = 0.0;
+        if (array.length === 0) {
+            for (var j = 0; j < rows * cols; j++) {
+                this.data[j] = 0.0;
+            }
         }
-    }
-    else if(array.length === rows*cols){
-        for(var i = 0; i < rows*cols; i++){
-            this.data[i] = array[i];
+        else if (array.length === rows * cols) {
+            for (var i = 0; i < rows * cols; i++) {
+                this.data[i] = array[i];
+            }
         }
-    }
-    else{
-        throw new Error("The array.length !== rows*cols");
+        else {
+            throw new Error("The array.length !== rows*cols");
 
-    }
-},
+        }
+    },
 
-    dot: function(leftMatrix, rightMatrix, out) {
+    dot: function (leftMatrix, rightMatrix, out) {
         var lrows = 1, lcols = 1, rrows = 1, rcols = 1;
 
         if (!leftMatrix || !rightMatrix) {
@@ -81,30 +81,30 @@ var numjs_linalg = {
         return out;
     },
 
-    matrix_power: function(matrix, n){
-    if(n===1){
-        return matrix;
-    }
+    matrix_power: function (matrix, n) {
+        if (n === 1) {
+            return matrix;
+        }
 
-    if(!matrix || !(matrix instanceof numjs_linalg.Matrix)){
-        throw new Error("The first arg must be instanceof numjs.Matrix");
-    }
-
-    if(matrix.rows !== matrix.cols){
-        throw new Error("matrix must be square, i.e. M.rows == M.cols");
-    }
-
-    var out = new numjs_linalg.Matrix([], matrix.rows, matrix.cols);
-    linalg.matrix_power(matrix.data, matrix.rows, matrix.cols, n, out.data);
-    return out;
-},
-
-    inv: function(matrix){
-        if(!matrix || !(matrix instanceof numjs_linalg.Matrix)){
+        if (!matrix || !(matrix instanceof numjs_linalg.Matrix)) {
             throw new Error("The first arg must be instanceof numjs.Matrix");
         }
 
-        if(matrix.rows !== matrix.cols){
+        if (matrix.rows !== matrix.cols) {
+            throw new Error("matrix must be square, i.e. M.rows == M.cols");
+        }
+
+        var out = new numjs_linalg.Matrix([], matrix.rows, matrix.cols);
+        linalg.matrix_power(matrix.data, matrix.rows, matrix.cols, n, out.data);
+        return out;
+    },
+
+    inv: function (matrix) {
+        if (!matrix || !(matrix instanceof numjs_linalg.Matrix)) {
+            throw new Error("The first arg must be instanceof numjs.Matrix");
+        }
+
+        if (matrix.rows !== matrix.cols) {
             throw new Error("matrix must be square, i.e. M.rows == M.cols");
         }
 
@@ -113,7 +113,7 @@ var numjs_linalg = {
         return out;
     },
 
-    det: function(matrix) {
+    det: function (matrix) {
         if (!matrix || !(matrix instanceof numjs_linalg.Matrix)) {
             throw new Error("The first arg must be instanceof numjs.Matrix");
 
@@ -125,7 +125,7 @@ var numjs_linalg = {
         return linalg.det(matrix.data, matrix.rows, matrix.cols);
     },
 
-    matrix_rank: function(matrix, tol) {
+    matrix_rank: function (matrix, tol) {
         if (!matrix || !(matrix instanceof numjs_linalg.Matrix)) {
             throw new Error("The first arg must be instanceof numjs.Matrix");
 
@@ -136,6 +136,43 @@ var numjs_linalg = {
         else {
             return linalg.matrix_rank(matrix.data, matrix.rows, matrix.cols);
         }
+    },
+
+    matrix_eigen_values: function (matrix, tol) {
+        if (!matrix || !(matrix instanceof numjs_linalg.Matrix)) {
+            throw new Error("The first arg must be instanceof numjs.Matrix");
+        }
+        if (matrix.rows !== matrix.cols) {
+            throw new Error("matrix must be square, i.e. M.rows == M.cols");
+        }
+        var eigenTestResult = new Float64Array(2 * matrix.cols);
+        linalg.get_eigen_values(matrix.rows, matrix.cols, matrix.data, eigenTestResult);
+        return eigenTestResult;
+    },
+
+    matrix_solve_linear: function (matrix, b) {
+        if (!matrix || !(matrix instanceof numjs_linalg.Matrix)) {
+            throw new Error("The first arg must be instanceof numjs.Matrix");
+        }
+        if (b.length !== matrix.cols) {
+            throw new Error("matrix and the parameter dimensions must agree.");
+        }
+        //TODO: check matrix rank ?
+        var x = new Float64Array(matrix.cols);
+        linalg.solve_linear_system_householder_qr(matrix.rows, matrix.cols, matrix.data, b, x);
+        return x;
+    },
+
+    matrix_mul: function (matrixA, matrixB) {
+        if (!matrixA || !(matrixA instanceof numjs_linalg.Matrix) || !matrixB || !(matrixB instanceof numjs_linalg.Matrix)) {
+            throw new Error("The first arg must be instanceof numjs.Matrix");
+        }
+        if (matrixA.cols !== matrixB.rows) {
+            throw new Error("matrix and the parameter dimensions must agree, i.e. matrixA.cols == matrixB.rows");
+        }
+        var res = new Float64Array(matrixA.rows * matrixB.cols);
+        linalg.mat_mul(matrixA.rows, matrixA.cols, matrixA.data, matrixB.rows, matrixB.cols, matrixB.data, res);
+        return new numjs_linalg.Matrix(res, matrixA.rows, matrixB.cols);
     }
 };
 
--- test/numjs.linalg.js
+++ test/numjs.linalg.js
@@ -2,7 +2,9 @@ var assert = require('chai').assert,
     expect = require('chai').expect,
     linalg = require('../numjs.linalg');
 
-describe('numjs.linalg', function() {
+var EPS = 0.1; //TODO: is there a built in check ?
+
+describe('numjs.linalg - native extension to support linear algebra via the Eigen cpp library', function() {
     describe('dot', function () {
         it('should return the product of two numbers', function () {
             assert.equal(linalg.dot(7, 3), 21);
@@ -209,4 +211,49 @@ describe('numjs.linalg', function() {
             expect(matrix.data["3"]).to.equal(1);
         })
     })
-});
+
+    describe('matrix_eigen_values', function () {
+        it('Should calculate the eigen values of a matrix \n\t\t\t({{1,2,3},{4,5,6},{7,8,9}})', function () {
+            var da = new Float64Array([1, 2, 3, 4, 5, 6, 7, 8, 9]);
+            var mat = new linalg.Matrix(da, 3, 3);
+            var res = linalg.matrix_eigen_values(mat);
+            expect(res[0]).to.be.within(16.1, 16.2);
+            expect(res[1]).to.be.within(0.0, EPS);
+            expect(res[2]).to.be.within(-1.2, -1.1);
+            expect(res[3]).to.be.within(0.0, EPS);
+            expect(res[4]).to.be.within(-EPS, EPS);
+            expect(res[5]).to.be.within(0.0, EPS);
+        })
+    })
+
+    describe('matrix_solve_linear', function () {
+        it('Should find the solution for \n\t\t\t({{0.8147,0.9134,0.2785},{0.9058,0.6324,0.5469},{0.1270,0.0975,0.9575}} X = {1,2,3})', function () {
+            var da = new Float64Array([0.8147, 0.9058, 0.1270, 0.9134, 0.6324, 0.0975, 0.2785, 0.5469, 0.9575]);
+            var mat = new linalg.Matrix(da, 3, 3);
+            var b = new Float64Array([1, 2, 3]);
+            res = linalg.matrix_solve_linear(mat, b);
+            expect(res[0]).to.be.within(0.62 - EPS, 0.62 + EPS);
+            expect(res[1]).to.be.within(-0.4 - EPS, -0.4 + EPS);
+            expect(res[2]).to.be.within(3.0 - EPS, 3.0 + EPS);
+        })
+    })
+
+    describe('matrix_mul', function () {
+        it('Should multiply  \n\t\t\t({{0.8147,0.9134,0.2785},{0.9058,0.6324,0.5469},{0.1270,0.0975,0.9575}}*\n\t\t\t{{0.9649,0.9572,0.1419},{0.1576,0.4854,0.4218},{0.9706,0.8003,0.9157}})', function () {
+            var da1 = new Float64Array([0.8147, 0.9058, 0.1270, 0.9134, 0.6324, 0.0975, 0.2785, 0.5469, 0.9575]);
+            var da2 = new Float64Array([0.9649, 0.1576, 0.9706, 0.9572, 0.4854, 0.8003, 0.1419, 0.4218, 0.9157]);
+            var A = new linalg.Matrix(da1, 3, 3);
+            var B = new linalg.Matrix(da2, 3, 3);
+            var res = linalg.matrix_mul(A, B);
+            expect(res.data[0]).to.be.within(1.2 - EPS, 1.2 + EPS);
+            expect(res.data[1]).to.be.within(1.5 - EPS, 1.5 + EPS);
+            expect(res.data[2]).to.be.within(1.0 - EPS, 1.0 + EPS);
+            expect(res.data[3]).to.be.within(1.4 - EPS, 1.4 + EPS);
+            expect(res.data[4]).to.be.within(1.6 - EPS, 1.6 + EPS);
+            expect(res.data[5]).to.be.within(0.9 - EPS, 0.9 + EPS);
+            expect(res.data[6]).to.be.within(0.7 - EPS, 0.7 + EPS);
+            expect(res.data[7]).to.be.within(0.8 - EPS, 0.8 + EPS);
+            expect(res.data[8]).to.be.within(0.9 - EPS, 0.9 + EPS);
+        })
+    })
+})
